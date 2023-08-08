158fab0628d8b166169d032104eee3164243e5f6
João Gabriel <joao.freiregabriel.2006@gmail.com>
2023-08-08 09:08:37 -0300

    commit aprofundamento em python 2

--- operadores/python_operadores.py
+++ operadores/python_operadores.py
@@ -1,6 +1,7 @@
 a = 3
 b = -4
 
+# Operadores Aritméticos
 soma = a + b
 sub = a - b
 mult = a * b
@@ -14,3 +15,37 @@ print(f"Divisão: {div}")
 print(f"Divisão de inteiros: {div_inteiros}")
 print(f"Cálculos dentro do print: {a * b}")
 print(f"Resto da divisão de 16 por a: {16 % a}")
+
+# Operadores Relacionais
+
+print(f"a == b: {a == b}")
+print(f"a < 5: {a < 5}")
+print(f"a > b: {a > b}")
+print(f"a <= 3: {a <= 3}")
+print(f"a >= 4: {a >= 4}")
+print(f"a != b: {a != b}")
+
+c = a != b
+
+print(f"c: {c}")
+
+#Operadores lógicos
+
+logic1 = True
+logic2 = False
+print(type(logic1))
+print(type(logic2))
+
+print(logic1)
+print(not logic1)
+print(logic1 or logic2)
+print(logic1 and logic2)
+
+print(f"logic1: {logic1}")
+print(f"not logic1: {not logic1}")
+print(f"logic1 or logic2: {logic1 or logic2}")
+print(f"logic1 and logic2: {logic1 and logic2}")
+
+
+
+
